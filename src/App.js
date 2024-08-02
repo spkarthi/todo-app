e1dede89652549c9b4e1dc11dad1671a3942e41f
Karthik Swamyayya <Karthik_Swamyayya@eogresources.com>
2024-08-02 00:58:48 -0500

    todo app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import TodoList from "./components/TodoList";
+import "./App.css";
+import Header from "./components/Header";
+import TodoForm from "./components/TodoForm"; 
 
-function App() {
+const App = () => {
+  const [todos, setTodos] = useState([
+    { id: 1, text: "Learn React", completed: false },
+    { id: 2, text: "Build a Todo List", completed: false },
+  ]);
+
+  const addTodo = (text) => {
+    const newTodo = { id: Date.now(), text: String(text), completed: false };
+    setTodos([...todos, newTodo]);
+  };
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const toggleComplete = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Header />
+      <TodoForm onAddTodo={addTodo} />
+      <TodoList
+        todos={todos}
+        onDelete={deleteTodo}
+        onToggleComplete={toggleComplete}
+      />
+      {/* Add a form or input to add new todos */}
     </div>
   );
-}
+};
 
 export default App;
